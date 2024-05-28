49c95cc19b29caa3e4320d45184ac25a648cc87f
soniagolden6 <Soniagolden6@gmail.com>
2024-05-27 21:36:13 -0400

    Commiting first program

---
+++ AIDI/Lab-1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Welcome to the division calculator')
+a = int(input('Please enter first digit:'))
+b = int(input('Please enter the second digit to divide:'))
+c = a/b
+
+print("Your results is",c)
+
+
+# In[ ]:
+
+
+
+
